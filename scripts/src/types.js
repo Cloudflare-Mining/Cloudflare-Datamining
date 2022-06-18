d293a26396dc7a1805d6102815458b9d7d2bbca4
James Ross <james@jross.me>
2022-06-19 00:40:17 +0100

    feat: add `@cloudflare/types` mining

---
+++ scripts/src/types.js
@@ -0,0 +1,31 @@
+import 'dotenv/config';
+import pacote from 'pacote';
+import path from 'path';
+import dateFormat from 'dateformat';
+import del from 'del';
+
+import {tryAndPush} from './utils.js';
+
+async function run(){
+	console.log('Fetching Cloudflare types...');
+
+	await pacote.extract('@cloudflare/types', path.resolve('../data/types'));
+	// This definitely isn't battle-hardended, but good enough for this
+	const delPaths = ['../data/types/dist', '../data/types/es', '../data/types/lib'].map(delPath => path.resolve(delPath));
+	await del(delPaths, {
+		force: true,
+	});
+
+	console.log('Pushing!');
+	const prefix = dateFormat(new Date(), 'd mmmm yyyy');
+	await tryAndPush(
+		['data/types/*'],
+		`${prefix} - @cloudflare/types Data was updated!`,
+		'CFData - @cloudflare/types Data Update',
+		'Pushed @cloudflare/types Data: ' + prefix,
+	);
+
+	console.log('Done! :)');
+}
+
+run();
