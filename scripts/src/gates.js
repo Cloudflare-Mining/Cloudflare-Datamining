7ec5eb8d42d5f64f86fd0e53fbb235084abcbf20
James Ross <james@jross.me>
2022-10-03 20:33:41 +0100

    feat: extend gates tracking

--- scripts/src/gates.js
+++ scripts/src/gates.js
@@ -6,21 +6,33 @@ import fetch from 'node-fetch';
 
 import {tryAndPush} from './utils.js';
 
-
+const getGates = [
+	{
+		key: process.env.GATES_API_KEY,
+		name: 'dashboard',
+	},
+	{
+		key: process.env.GATES_ZT_API_KEY,
+		name: 'zt-dashboard',
+	},
+];
 async function run(){
 	await fs.ensureDir(path.resolve(`../data/gates`));
-	// get gates
-	const config = await fetch("https://gates.cloudflare.com/api/v1/runtime/configuration", {
-		"headers": {
-			"authorization": `Bearer ${process.env.GATES_API_KEY}`,
-			"Referer": "https://dash.cloudflare.com/",
-		},
-	});
-	if(config.status === 200){
-		const json = await config.json();
-		if(json.success){
-			console.log('Writing gates config');
-			await fs.writeFile(path.resolve('../data/gates/config.json'), JSON.stringify(json.result, null, '\t'));
+	for(const gate of getGates){
+		const file = path.resolve(`../data/gates/${gate.name}.json`);
+
+		const config = await fetch("https://gates.cloudflare.com/api/v1/runtime/configuration", {
+			"headers": {
+				"authorization": `Bearer ${gate.key}`,
+				"Referer": "https://dash.cloudflare.com/",
+			},
+		});
+		if(config.status === 200){
+			const json = await config.json();
+			if(json.success){
+				console.log('Writing gates config', gate.name);
+				await fs.writeFile(file, JSON.stringify(json.result, null, '\t'));
+			}
 		}
 	}
 
