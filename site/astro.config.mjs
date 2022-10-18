6b81a773112f4932acc1c53c4fbd139842f7a42c
James Ross <james@jross.me>
2022-10-18 23:29:38 +0100

    chore: eslint fixes

--- site/astro.config.mjs
+++ site/astro.config.mjs
@@ -5,9 +5,11 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://cfdata.lol',
 	integrations: [
 		tailwind(),
 		robotsTxt({
+			sitemap: false,
 			policy: [{
 				userAgent: '*',
 				allow: '/',
