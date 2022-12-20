0f06a20414146636527233cca6c24b6fde367705
James Ross <james@jross.me>
2022-12-20 23:58:47 +0000

    feat: track gates assignment percentages

--- scripts/src/gates.js
+++ scripts/src/gates.js
@@ -4,7 +4,9 @@ import fs from 'fs-extra';
 import dateFormat from 'dateformat';
 import fetch from 'node-fetch';
 
-import {tryAndPush} from './utils.js';
+import {tryAndPush, getHttpsAgent} from './utils.js';
+
+const agent = getHttpsAgent();
 
 const getGates = [
 	{
@@ -22,9 +24,10 @@ async function run(){
 		const file = path.resolve(`../data/gates/${gate.name}.json`);
 
 		const config = await fetch("https://gates.cloudflare.com/api/v1/runtime/configuration", {
-			"headers": {
-				"authorization": `Bearer ${gate.key}`,
-				"Referer": "https://dash.cloudflare.com/",
+			agent,
+			headers: {
+				Authorization: `Bearer ${gate.key}`,
+				Referer: "https://dash.cloudflare.com/",
 			},
 		});
 		if(config.status === 200){
@@ -36,6 +39,65 @@ async function run(){
 		}else{
 			console.log('Error fetching gates config', gate.name);
 		}
+
+		// get assignment percentage (rough)
+		const assignments = {};
+		const fetchCount = 1000;
+		for(let i = 0; i < fetchCount; i++){
+			console.log('Check assignments', gate.name, i);
+			const assignment = await fetch("https://gates.cloudflare.com/api/v1/runtime/assignments", {
+				agent,
+				headers: {
+					Authorization: `Bearer ${gate.key}`,
+					Referer: "https://dash.cloudflare.com/",
+				},
+				method: 'POST',
+				body: JSON.stringify({
+					userId: String(i),
+					accountId: String(i),
+					is_ent: false,
+				}),
+			});
+			if(assignment.status === 200){
+				const json = await assignment.json();
+				if(json.success){
+					const assignment = json.result;
+					for(const gate in assignment){
+						// only count if it's a boolean
+						assignments[gate] ??= {};
+						assignments[gate][assignment[gate]] ??= 0;
+						assignments[gate][assignment[gate]]++;
+					}
+				}
+			}
+		}
+		// try and normalize the percentages
+		for(const gate in assignments){
+			// normalise 30%, 50%, etc.
+			const values = Object.values(assignments[gate]);
+			const min = Math.min(...values);
+			const max = Math.max(...values);
+			if(Object.keys(assignments[gate]).length === 2 && min >= 480 && max <= 520){
+				assignments[gate] = {
+					[Object.keys(assignments[gate])[0]]: Math.round(fetchCount / 2),
+					[Object.keys(assignments[gate])[1]]: Math.round(fetchCount / 2),
+				};
+			}else if(Object.keys(assignments[gate]).length === 3 && min >= 310 && max <= 350){
+				assignments[gate] = {
+					[Object.keys(assignments[gate])[0]]: Math.round(fetchCount / 3),
+					[Object.keys(assignments[gate])[1]]: Math.round(fetchCount / 3),
+					[Object.keys(assignments[gate])[2]]: Math.round(fetchCount / 3),
+				};
+			}
+			// normalise to 100%
+			const total = Object.values(assignments[gate]).reduce((itemA, itemB) => itemA + itemB, 0);
+			for(const key in assignments[gate]){
+				assignments[gate][key] = Math.round((assignments[gate][key] / total) * 100);
+			}
+		}
+		const assignmentsFile = path.resolve(`../data/gates/${gate.name}-assignments.json`);
+		await fs.writeFile(assignmentsFile, JSON.stringify(assignments, null, '\t'));
+
 	}
 
 	console.log('Pushing!');
