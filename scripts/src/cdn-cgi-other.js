24ff2ddcc2fffbb9bc44b12fc1eb56fc8e6327ca
James Ross <james@jross.me>
2023-01-30 23:52:55 +0000

    feat: mine Functions request.cf
    feat: split request.cf and trace into separate channel

---
+++ scripts/src/cdn-cgi-other.js
@@ -0,0 +1,66 @@
+import 'dotenv/config';
+import path from 'node:path';
+import fs from 'fs-extra';
+import fetch from 'node-fetch';
+import dateFormat from 'dateformat';
+
+import {tryAndPush, propertiesToArray} from './utils.js';
+
+const dir = path.resolve(`../data/cdn-cgi`);
+await fs.ensureDir(dir);
+
+// get keys for trace
+const traceRes = await fetch(`https://colo.quest/cdn-cgi/trace`);
+const trace = await traceRes.text();
+const data = trace.split('\n').map(line => line.split('='));
+const traceKeys = data.map(arr => arr[0]).filter(key => key !== '').sort();
+if(traceKeys.length >= 0){
+	await fs.writeFile(path.resolve(dir, 'trace.json'), JSON.stringify([...traceKeys].sort(), null, '\t'));
+}
+
+// add keys that are optional, and assume they're set. This creates more stable diffs
+function fixSet(cfKeys){
+	cfKeys.add('clientTcpRtt');
+	cfKeys.add('isEUCountry');
+	cfKeys.add('country');
+	cfKeys.add('city');
+	cfKeys.add('continent');
+	cfKeys.add('latitude');
+	cfKeys.add('longitude');
+	cfKeys.add('postalCode');
+	cfKeys.add('metroCode');
+	cfKeys.add('region');
+	cfKeys.add('regionCode');
+	cfKeys.add('timezone');
+	return cfKeys;
+}
+
+// get keys for request.cf
+const cfJsonRes = await fetch(`https://jross.me/cf.json`);
+const cfJson = await cfJsonRes.json();
+const cfKeys = new Set(propertiesToArray(cfJson));
+fixSet(cfKeys);
+if(cfKeys.size >= 0){
+	await fs.writeFile(path.resolve(dir, 'cf.json'), JSON.stringify([...cfKeys].sort(), null, '\t'));
+}
+
+// get keys for request.cf in functions
+const cfFunctionsJsonRes = await fetch(`https://cfjson.pages.dev/cf.json`);
+const cfFunctionsJson = await cfFunctionsJsonRes.json();
+const cfFunctionsKeys = new Set(propertiesToArray(cfFunctionsJson));
+
+fixSet(cfFunctionsKeys);
+if(cfFunctionsKeys.size >= 0){
+	await fs.writeFile(path.resolve(dir, 'cf.functions.json'), JSON.stringify([...cfFunctionsKeys].sort(), null, '\t'));
+}
+
+const prefix = dateFormat(new Date(), 'd mmmm yyyy');
+await tryAndPush(
+	[
+		'data/cdn-cgi/**/*.json',
+	],
+	`${prefix} - request.cf/trace Data was updated! [skip ci]`,
+	'CFData - Request.cf/trace Update',
+	'Pushed Request.cf/trace: ' + prefix,
+	'DISCORD_WEBHOOK_CDNCGI_OTHER',
+);
