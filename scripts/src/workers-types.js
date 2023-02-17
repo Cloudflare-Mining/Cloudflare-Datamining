ffd69e5cdd3ef6265d2cb5ae5f9ddafec4a96280
James Ross <james@jross.me>
2023-02-17 21:01:47 +0000

    feat: mine workers-types for diffs

---
+++ scripts/src/workers-types.js
@@ -0,0 +1,25 @@
+import 'dotenv/config';
+import pacote from 'pacote';
+import path from 'node:path';
+import dateFormat from 'dateformat';
+
+import {tryAndPush} from './utils.js';
+
+async function run(){
+	console.log('Fetching Cloudflare Workers Types...');
+
+	await pacote.extract('@cloudflare/workers-types', path.resolve('../data/workers-types'));
+
+	console.log('Pushing!');
+	const prefix = dateFormat(new Date(), 'd mmmm yyyy');
+	await tryAndPush(
+		['data/workers-types/*'],
+		`${prefix} - @cloudflare/workers-types Data was updated!`,
+		'CFData - @cloudflare/workers-types Data Update',
+		'Pushed @cloudflare/workers-types Data: ' + prefix,
+	);
+
+	console.log('Done! :)');
+}
+
+run();
