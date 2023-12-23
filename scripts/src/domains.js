0078204d58d466439472351882c1639e8cf20fef
James Ross <james@jross.me>
2023-12-23 19:35:55 +0000

    chore: eslint fixups

--- scripts/src/domains.js
+++ scripts/src/domains.js
@@ -1,12 +1,13 @@
 import 'dotenv/config';
 import path from 'node:path';
-import fs from 'fs-extra';
-import fetch from 'node-fetch';
+
 import dateFormat from 'dateformat';
+import fs from 'fs-extra';
 import ipRegex from 'ip-regex';
+import fetch from 'node-fetch';
 
 
-import {tryAndPush, getHttpsAgent} from './utils.js';
+import { getHttpsAgent, tryAndPush } from './utils.js';
 
 const agent = getHttpsAgent();
 
@@ -15,25 +16,25 @@ await fs.ensureDir(dir);
 
 const payloads = [
 	{
-		'apiKey': process.env.WHOISXMLAPI_KEY,
-		'sinceDate': '2009-01-01',
-		'mode': 'purchase',
-		'punycode': true,
-		'advancedSearchTerms': [{
-			'field': 'RegistrantContact.Organization',
-			'term': 'Cloudflare, Inc.',
-			'exactMatch': false,
+		apiKey: process.env.WHOISXMLAPI_KEY,
+		sinceDate: '2009-01-01',
+		mode: 'purchase',
+		punycode: true,
+		advancedSearchTerms: [{
+			field: 'RegistrantContact.Organization',
+			term: 'Cloudflare, Inc.',
+			exactMatch: false,
 		}],
 	},
 	{
-		'apiKey': process.env.WHOISXMLAPI_KEY,
-		'sinceDate': '2009-01-01',
-		'mode': 'purchase',
-		'punycode': true,
-		'advancedSearchTerms': [{
-			'field': 'RegistrantContact.Email',
-			'term': 'domains@cloudflare.com',
-			'exactMatch': true,
+		apiKey: process.env.WHOISXMLAPI_KEY,
+		sinceDate: '2009-01-01',
+		mode: 'purchase',
+		punycode: true,
+		advancedSearchTerms: [{
+			field: 'RegistrantContact.Email',
+			term: 'domains@cloudflare.com',
+			exactMatch: true,
 		}],
 	},
 ];
@@ -43,32 +44,37 @@ const domainsRes = [
 		method: 'POST',
 		body: JSON.stringify(payloads[0]),
 	}),
+	await fetch('https://registrant-alert.whoisxmlapi.com/api/v2', {
+		agent,
+		method: 'POST',
+		body: JSON.stringify(payloads[1]),
+	}),
 ];
 
 let activeDomains = new Set([]);
 let droppedDomains = new Set([]);
-try{
+try {
 	activeDomains = new Set(await fs.readJson(path.resolve('../data/domains/domains-active.json')));
 	droppedDomains = new Set(await fs.readJson(path.resolve('../data/domains/domains-dropped.json')));
-}catch{}
-for(const res of domainsRes) {
-	if(res.ok) {
+} catch {}
+for (const res of domainsRes) {
+	if (res.ok) {
 		const rawDomains = await res.json();
-		for(const domain of (rawDomains?.domainsList || [])) {
-			if(!ipRegex().test(domain.domainName)) {
-				if(domain.action === 'added' || domain.action === 'discovered') {
+		for (const domain of (rawDomains?.domainsList || [])) {
+			if (!ipRegex().test(domain.domainName)) {
+				if (domain.action === 'added' || domain.action === 'discovered') {
 					activeDomains.add(domain.domainName);
-				}else if(domain.action === 'dropped') {
+				} else if (domain.action === 'dropped') {
 					droppedDomains.add(domain.domainName);
 				}
 			}
 		}
 	}
 }
-if(activeDomains.size > 0) {
+if (activeDomains.size > 0) {
 	await fs.writeFile(path.resolve(dir, 'domains-active.json'), JSON.stringify([...activeDomains].sort(), null, '\t'));
 }
-if(droppedDomains.size > 0) {
+if (droppedDomains.size > 0) {
 	await fs.writeFile(path.resolve(dir, 'domains-dropped.json'), JSON.stringify([...droppedDomains].sort(), null, '\t'));
 }
 
