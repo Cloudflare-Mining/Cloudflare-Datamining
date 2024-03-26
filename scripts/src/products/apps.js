959553efffb12c7b6f7c92822e3c663980cd13ea
James Ross <james@jross.me>
2024-03-26 19:40:55 +0000

    fix: catch apps endpoints timing out

--- scripts/src/products/apps.js
+++ scripts/src/products/apps.js
@@ -12,33 +12,24 @@ const dir = path.resolve('../data/products/apps');
 await fs.ensureDir(dir);
 await fs.emptyDir(dir);
 
-const apps = await fetch('https://api.cloudflareapps.com/apps/');
-const appsJson = await apps.json();
 
-const ratePlans = new Set([]);
+try {
+	const apps = await fetch('https://api.cloudflareapps.com/apps/');
+	const appsJson = await apps.json();
 
-for (const app of appsJson) {
-	const filename = filenamify(`${app.title} - ${app.id}`);
-	const appDir = path.resolve(dir, filename);
-	await fs.ensureDir(appDir);
+	const ratePlans = new Set([]);
+	for (const app of appsJson) {
+		const filename = filenamify(`${app.title} - ${app.id}`);
+		const appDir = path.resolve(dir, filename);
+		await fs.ensureDir(appDir);
 
-	console.log('Get app info', app.id, app.title);
-	const appInfo = await fetch(`https://api.cloudflareapps.com/apps/${app.id}`);
-	const appsJson = await appInfo.json();
-	const file = path.resolve(appDir, 'info.json');
-	// delete some fields we don't care for
-	const json = appsJson;
-	for (const product of json.products || []) {
-		if (product.plan) {
-			ratePlans.add(product.plan.toLowerCase());
-		}
-		if (product.feature && !product.feature.includes('.')) {
-			ratePlans.add(product.feature.toLowerCase());
-		}
-	}
-	for (const version of json.versions) {
-		delete version.ratingSummary;
-		for (const product of version?.definition?.products || []) {
+		console.log('Get app info', app.id, app.title);
+		const appInfo = await fetch(`https://api.cloudflareapps.com/apps/${app.id}`);
+		const appsJson = await appInfo.json();
+		const file = path.resolve(appDir, 'info.json');
+		// delete some fields we don't care for
+		const json = appsJson;
+		for (const product of json.products || []) {
 			if (product.plan) {
 				ratePlans.add(product.plan.toLowerCase());
 			}
@@ -46,18 +37,32 @@ for (const app of appsJson) {
 				ratePlans.add(product.feature.toLowerCase());
 			}
 		}
+		for (const version of json.versions) {
+			delete version.ratingSummary;
+			for (const product of version?.definition?.products || []) {
+				if (product.plan) {
+					ratePlans.add(product.plan.toLowerCase());
+				}
+				if (product.feature && !product.feature.includes('.')) {
+					ratePlans.add(product.feature.toLowerCase());
+				}
+			}
+		}
+		delete json.ratingSummary;
+		await fs.writeJson(file, json, { spaces: '\t' });
 	}
-	delete json.ratingSummary;
-	await fs.writeJson(file, json, { spaces: '\t' });
+
+	await fs.writeJson(path.resolve(dir, 'rate-plans.json'), [...ratePlans].sort(), { spaces: '\t' });
+	const prefix = dateFormat(new Date(), 'd mmmm yyyy');
+	await tryAndPush(
+		[
+			'data/products/apps/*',
+		],
+		`${prefix} - Product: Apps was updated!`,
+		'CFData - Product: Apps Update',
+		'Pushed Product: Apps Data: ' + prefix,
+	);
+} catch (err) {
+	console.error(err);
 }
 
-await fs.writeJson(path.resolve(dir, 'rate-plans.json'), [...ratePlans].sort(), { spaces: '\t' });
-const prefix = dateFormat(new Date(), 'd mmmm yyyy');
-await tryAndPush(
-	[
-		'data/products/apps/*',
-	],
-	`${prefix} - Product: Apps was updated!`,
-	'CFData - Product: Apps Update',
-	'Pushed Product: Apps Data: ' + prefix,
-);
